3bd60676ebf1511a9e4897977a7cdab072c82183
Ismailouab <157055561+Ismailouab@users.noreply.github.com>
2024-03-17 13:51:23 +0000

    add some lines in javascript

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ var me;
 var myscore=0;
 var opponent;
 var opponentscore=0;
+var choices=["rock","paper","scissors"];
 
-var choices=["rock","paper","scissors"]
+window.onload=function(){
+    for(let i=0; i<3; i++){
+        let choice=document.createElement("img");
+        choice.id=choices[i];
+        choice.src=choices[i]+ ".png";
+        choice.addEventListener("click",khtar);
+        document.getElementById("choices").append(choice);
+
+    }
+}
