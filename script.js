38e802cc7ac119b08e3f58ff2b47cf43734bf8be
Ismailouab <157055561+Ismailouab@users.noreply.github.com>
2024-03-18 17:40:50 +0000

    made some changes in game

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
 var me;
 var myscore=0;
-var opponent;
-var opponentscore=0;
+var you;
+var youscore=0;
 var choices=["rock","paper","scissors"];
 
 window.onload=function(){
     for(let i=0; i<3; i++){
-        let choice=document.createElement("img");
-        choice.id=choices[i];
-        choice.src=choices[i]+ ".png";
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i]+ ".png";
         choice.addEventListener("click",khtar);
         document.getElementById("choices").append(choice);
 
     }
+}
+
+function khtar(){
+    me=this.id;
+    document.getElementById("my-choice").src= me + ".png";
+
+    you = choices[Math.floor(Math.random()*3)];
+    document.getElementById("you-choice").src = you + ".png";
+
+    if(me==you){
+        myscore+=1;
+        youscore+=1;
+    }
+    else{
+        if(me=="rock"){
+            if(you=="scissors"){
+                myscore+=1;
+            }
+            else if(you=="paper"){
+                youscore+=1;
+            }
+        }
+        else if(me=="scissors"){
+            if(you=="paper"){
+                myscore+=1;
+            }
+            else if(you=="rock"){
+                youscore+=1;
+            }
+        }
+         else if(me=="paper"){
+            if(you=="rock"){
+                myscore+=1;
+            }
+            else if(you=="scissors"){
+                youscore+=1;
+            }
+        }
+    }
+document.getElementById("my-score").innerHTML= myscore;
+document.getElementById("you-score").innerHTML= youscore;
+    
 }
